e07c3261f1b0fb3d3daa8a15038aef619d7e2e84
Schwartz <darlanswtz@gmail.com>
2023-09-08 17:15:35 -0300

    Feat: restructured collatreals and loans page, also some style changes

--- frontend/src/Pages/Components/Blockchain/BlockchainCurrency.jsx
+++ frontend/src/Pages/Components/Blockchain/BlockchainCurrency.jsx
@@ -1,21 +1,47 @@
 import React from 'react'
 import { styled } from 'styled-components'
+import CollateralSlideBar from '../Collaterals/CollateralSlideBar';
 
-export default function BlockchainCurrency({ name, image, price, units, total_value }) {
+export default function BlockchainCurrency({show_actions=false, name='', image='/placeholder.svg', price=0, units=0, total_value=0,total_used=0,onCheckout,onStartLoan }) {
   return (
     <Container>
+      <AssetInfo>
       <NameLogo>
-        <img src={image ? image : '/placeholder.svg'} alt={name} />
-        <h1>{name ? name : '-----'}</h1>
+        <img src={image} alt={name} />
+        <h1>{name}</h1>
       </NameLogo>
       <MainInfo>
-        <p className='price'>{`${price ? 'R$ ' + price : '----'}`}</p>
-        <p className='units'>{`${units ? units : '---'}`}</p>
-        <p className='total'>{`${total_value ? 'R$ ' + total_value : '----'}`}</p>
+        <p className='price'>{`${'R$ ' + price}`}</p>
+        <p className='units'>{`${units}`}</p>
+        <p className='total'>{`${'R$ ' + total_value}`}</p>
       </MainInfo>
+      </AssetInfo>
+      <Actions>
+            <CollateralSlideBar value={total_used} />
+            {
+              show_actions &&
+              <div className='btns'>
+                <button onClick={()=> onStartLoan({name,image,price,units,total_value,total_used})}>Solicitar Empréstimo</button>
+                <button disabled={total_used == 100} onClick={()=> onCheckout({name,image,price,units,total_value,total_used})}>Sacar</button>
+              </div>
+            }
+          </Actions>
     </Container>
   )
 }
+const Actions = styled.nav`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+  margin-top: 1.94rem;
+  .btns{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1.88rem;
+  }
+`;
 
 const MainInfo = styled.div`
 
@@ -71,9 +97,16 @@ const NameLogo = styled.div`
   }
 `;
 
-const Container = styled.div`
+const AssetInfo = styled.div`
   align-items: center;
   display: flex;
   justify-content: space-between;
+  width: 100%;
+`;
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content:center;
+  flex-direction: column;
   width: 100%;
 `;

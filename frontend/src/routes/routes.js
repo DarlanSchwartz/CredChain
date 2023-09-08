3bc4e6653e2957335f04d4ee6c7629e3fa68db0b
Schwartz <darlanswtz@gmail.com>
2023-09-08 20:09:17 -0300

    Fix: routes.js url

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,6 +1,4 @@
-
-const API_URL = 'http://localhost:5000';
-//const API_URL = `${process.env.REACT_APP_API_URL}`
+const API_URL = process.env.REACT_APP_API_URL
 
 export const backendroute = {
     postSignUp: API_URL + '/signup',

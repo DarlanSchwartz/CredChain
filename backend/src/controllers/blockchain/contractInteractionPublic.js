69a677558c32d25a9661c6b1e90bc9a552bbbd10
chxpz <rpcweb3@gmail.com>
2023-09-11 00:53:37 -0300

    feat: added SC integration functions

---
+++ backend/src/controllers/blockchain/contractInteractionPublic.js
@@ -0,0 +1,93 @@
+import { getContract } from './utils';
+import { abi } from "../abis/credchainAbi.json"
+
+export const setAcceptedTokens = async (
+    privateKey,
+    providerUrl,
+    contractAddress,
+    tokenAddress,
+    isAccepted
+) => {
+    try {
+        const contract = getContract(privateKey, providerUrl, contractAddress, abi)
+        const tx = await contract.setAcceptedTokens(tokenAddress, isAccepted);
+        const receipt = await tx.wait();
+
+        const returnObject = {
+            "hash": tx.hash,
+            "status": receipt.status
+        }
+
+        return returnObject
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const releaseLockedTokens = async (
+    privateKey,
+    providerUrl,
+    contractAddress,
+    tokenAddress,
+    recipient,
+    user,
+    amount
+) => {
+    try {
+        const contract = getContract(privateKey, providerUrl, contractAddress, abi)
+        const tx = await contract.releaseLockedTokens(tokenAddress, recipient, user, amount);
+        const receipt = await tx.wait();
+
+        const returnObject = {
+            "hash": tx.hash,
+            "status": receipt.status
+        }
+
+        return returnObject
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const getBalanceOfTokensLockedByUser = async (
+    privateKey,
+    providerUrl,
+    contractAddress,
+    user,
+    tokenAddress
+) => {
+    try {
+        const contract = getContract(privateKey, providerUrl, contractAddress, abi)
+        const balance = await contract.getBalanceOf(user, tokenAddress);
+
+        return balance
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const getUsedCollateral = async (
+    privateKey,
+    providerUrl,
+    contractAddress,
+    tokenAddress,
+    user
+) => {
+    try {
+        const contract = getContract(privateKey, providerUrl, contractAddress, abi)
+        const usedCollateral = await contract.getCollateral(tokenAddress, user);
+
+        return usedCollateral
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
